a21d6e16db86eb601fb744e5e53227a976622b87
Ken Jennings <kj.sumter.42@gmail.com>
2018-01-05 23:04:06 -0500

    Update macro_math.asm

--- macro_math.asm
+++ macro_math.asm
@@ -5,6 +5,112 @@
 ;===============================================================================
 
 ;===============================================================================
+; 8-BIT MATH
+;===============================================================================
+; 
+;===============================================================================
+
+;-------------------------------------------------------------------------------
+;                                                         BYTE_M_EQ_V_SUB_V   A
+;-------------------------------------------------------------------------------
+; mByte_M_eq_V_Sub_V <result (address)>, <value1>, <value2>
+;
+; Subtract literal <Value2> from literal <Value1>, store in <Result (address)>.
+;
+; or 
+; result = Value1 - Value2
+; 
+; or (more exactly) 
+; Poke result, Value1 - Value2
+;-------------------------------------------------------------------------------
+.macro mByte_M_eq_V_Sub_V
+	.if %0<>3
+		.error "mByte_M_eq_V_Sub_V: 3 arguments (result addr, value1, value2) required."
+	.else
+	    sec        ; clear carry/borrow
+		lda #<%2   ; Low byte of Value1
+		sbc #<%3   ; subtract low byte of Value2
+		sta %1     ; save in Result
+	.endif
+.endm
+
+
+;-------------------------------------------------------------------------------
+;                                                         BYTE_M_EQ_M_SUB_V   A
+;-------------------------------------------------------------------------------
+; mByte_M_eq_M_Sub_V <result (address)>, <address>, <value>
+;
+; Subtract literal <Value> from value at <Address>, store in <Result (address)>.
+;
+; or 
+; result = address - Value
+; 
+; or (more exactly) 
+; Poke result, Peek(address) - Value 
+;-------------------------------------------------------------------------------
+.macro mByte_M_eq_M_Sub_V
+  .if %0<>3
+    .error "mByte_M_eq_M_Sub_V: 3 arguments (result addr, addr, value) required."
+  .else
+    sec        ; clear carry/borrow
+    lda %2     ; Low byte of Address
+    sbc #<%3   ; subtract low byte of Value
+    sta %1     ; save in Result
+	.endif
+.endm
+
+
+;-------------------------------------------------------------------------------
+;                                                         BYTE_M_EQ_V_SUB_M   A
+;-------------------------------------------------------------------------------
+; mByte_M_eq_M_Sub_V <result (address)>, <value>, <address>
+;
+; Subtract value at <Address> from literal <Value>, store in <Result (address)>.
+;
+; or 
+; result = Value - Address
+; 
+; or (more exactly) 
+; Poke result, Value - Peek(address)
+;-------------------------------------------------------------------------------
+.macro mByte_M_eq_M_Sub_V
+  .if %0<>3
+    .error "mByte_M_eq_M_Sub_V: 3 arguments (result addr, value, addr) required."
+  .else
+    sec        ; clear carry/borrow
+    lda #<%2   ; Low byte of Value
+    sbc %3     ; subtract low byte of Address
+    sta %1     ; save in Result
+	.endif
+.endm
+
+;-------------------------------------------------------------------------------
+;                                                         BYTE_M_EQ_M_SUB_M   A
+;-------------------------------------------------------------------------------
+; mByte_M_eq_M_Sub_V <result (address)>, <address1>, <address2>
+;
+; Subtract value at <address2> from value at <Address1>, store in <Result (address)>.
+;
+; or 
+; result = address1 - address2
+; 
+; or (more exactly) 
+; Poke result, Peek(address1) - Peek(address2) 
+;-------------------------------------------------------------------------------
+.macro mByte_M_eq_M_Sub_V
+  .if %0<>3
+    .error "mByte_M_eq_M_Sub_V: 3 arguments (result addr, addr1, addr2) required."
+  .else
+    sec        ; clear carry/borrow
+    lda %2     ; Low byte of Address
+    sbc %3     ; subtract low byte of Value
+    sta %1     ; save in Result
+	.endif
+.endm
+  
+
+
+;===============================================================================
 ; 16-BIT MATH
 ;===============================================================================
 ; 
